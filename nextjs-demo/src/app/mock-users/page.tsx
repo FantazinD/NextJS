7662ec0557e1aa55d07183b3e71f8ba293214229
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-11 01:36:06 +0800

    added form to mock-users page

--- nextjs-demo/src/app/mock-users/page.tsx
+++ nextjs-demo/src/app/mock-users/page.tsx
@@ -8,12 +8,20 @@ export default async function MockUsers() {
     const users = await response.json();
 
     return (
-        <ul className="space-y-4 p-4">
-            {users.map((user: MockUser) => (
-                <li key={user.id} className="p-4 bg-white shadow-md rounded-lg text-gray-700">
-                    {user.name}
-                </li>
-            ))}
-        </ul>
+        <div className="py-10">
+            <form className="ml-4 mb-4">
+                <input type="text" name="name" required className="border p-2 mr-2" />
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
+                    Add user
+                </button>
+            </form>
+            <div className="grid grid-cols-4 gap-4 py-10">
+                {users.map((user: MockUser) => (
+                    <div key={user.id} className="p-4 bg-white shadow-md rounded-lg text-gray-700">
+                        {user.name}
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
