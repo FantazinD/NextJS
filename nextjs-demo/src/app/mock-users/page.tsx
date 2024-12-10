7bc1a9dcf5f1c7cc78b5d5f65882f2c8828332f6
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-11 01:38:58 +0800

    added async function, addUser

--- nextjs-demo/src/app/mock-users/page.tsx
+++ nextjs-demo/src/app/mock-users/page.tsx
@@ -7,10 +7,26 @@ export default async function MockUsers() {
     const response = await fetch("https://6758788c60576a194d10ab21.mockapi.io/users");
     const users = await response.json();
 
+    async function addUser(formData: FormData) {
+        "use server";
+
+        const name = formData.get("name");
+        const response = await fetch("https://6758788c60576a194d10ab21.mockapi.io/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name }),
+        });
+
+        const newUser = await response.json();
+        console.log(newUser);
+    }
+
     return (
         <div className="py-10">
-            <form className="ml-4 mb-4">
-                <input type="text" name="name" required className="border p-2 mr-2" />
+            <form action={addUser} className="ml-4 mb-4">
+                <input type="text" name="name" required className="border p-2 mr-2 text-gray-700" />
                 <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
                     Add user
                 </button>
