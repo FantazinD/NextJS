03afec7fa245ebca8b1f0c9dd3cf0203b6ace0bf
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-12-13 03:39:18 +0800

    get userObj and authObj

--- nextjs-demo/src/app/mock-users/page.tsx
+++ nextjs-demo/src/app/mock-users/page.tsx
@@ -1,3 +1,4 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 type MockUser = {
@@ -6,6 +7,11 @@ type MockUser = {
 };
 
 export default async function MockUsers() {
+    const authObj = await auth();
+    const userObj = await currentUser();
+
+    console.log({ authObj, userObj });
+
     const response = await fetch("https://6758788c60576a194d10ab21.mockapi.io/users");
     const users = await response.json();
 
